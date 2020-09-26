ee78c9864d1bafad438103bb028c00fe6e146207
BaGreal2 <artemonik17@gmail.com>
2020-09-26 16:04:30 +0300

    Updates

---
+++ precache-manifest.aa979d98a8cc203b22a4c7107576b464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb8271602ba6e673c0e2275cfef73a1",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "a8e3b8cfb2cb00697712",
+    "url": "/goit-react-hw-01-components/static/css/main.c0216a9d.chunk.css"
+  },
+  {
+    "revision": "5343ff54f44e37243b47",
+    "url": "/goit-react-hw-01-components/static/js/2.e2831900.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.e2831900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e3b8cfb2cb00697712",
+    "url": "/goit-react-hw-01-components/static/js/main.4dba9cf2.chunk.js"
+  },
+  {
+    "revision": "787980e203a080b71791",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.6f525e94.js"
+  }
+]);
