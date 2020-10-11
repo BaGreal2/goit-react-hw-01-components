7800574c1d4d613691b191154056b5ad96101eae
BaGreal2 <artemonik17@gmail.com>
2020-10-11 10:40:08 +0300

    Updates

--- precache-manifest.ce455555453f3d0668a5d483ac3bc9d3.js
+++ precache-manifest.ce455555453f3d0668a5d483ac3bc9d3.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d79d227a799e97366a2b9519a0ff6545",
+    "revision": "4353af5fdd0f0af81fb8f82aee3fb94b",
     "url": "/goit-react-hw-01-components/index.html"
   },
   {
-    "revision": "0c99b1bfc33dde04c730",
-    "url": "/goit-react-hw-01-components/static/css/main.65724a29.chunk.css"
+    "revision": "9d166a0ab6d697b2cade",
+    "url": "/goit-react-hw-01-components/static/css/main.1eee7141.chunk.css"
   },
   {
     "revision": "5343ff54f44e37243b47",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-01-components/static/js/2.e2831900.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0c99b1bfc33dde04c730",
-    "url": "/goit-react-hw-01-components/static/js/main.127051ab.chunk.js"
+    "revision": "9d166a0ab6d697b2cade",
+    "url": "/goit-react-hw-01-components/static/js/main.201aabb3.chunk.js"
   },
   {
     "revision": "787980e203a080b71791",
