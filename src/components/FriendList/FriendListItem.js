0c19167767c13e35d72d6bff55b9ecf187df3661
BaGreal2 <artemonik17@gmail.com>
2020-10-13 12:38:02 +0300

    fixed 3

--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './FriendList.module.css';
 
-function FriendListItem({ avatar, name, isOnline, id }) {
+function FriendListItem({ avatar, name, isOnline}) {
   return (
-    <li className={styles.item} key={id}>
+    <li className={styles.item}>
       <span className={isOnline ? styles.green : styles.red} />
       <img className={styles.avatar} src={avatar} alt="" width="48" />
       <p className={styles.name}>{name}</p>
@@ -16,7 +16,6 @@ FriendListItem.propTypes = {
   avatar: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   isOnline: PropTypes.bool.isRequired,
-  id: PropTypes.number.isRequired,
 };
 
 export default FriendListItem;
